50b1bb80ee88cd386eb9b0af23875ecb8d819637
Omri Rajuan <107933333+Omri93Rajuan@users.noreply.github.com>
2024-11-20 10:20:35 +0200

    Adding PageHeader comp to HOME page

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,12 +1,14 @@
 import React, { useContext } from "react";
 import { AuthContext } from "../providers/AuthProvider";
 import { NavLink } from "react-router-dom";
+import PageHeader from "../components/PageHeader";
 
 const HomePage = () => {
   const { user, logout } = useContext(AuthContext) ?? {};
 
   return (
     <div>
+      <PageHeader title="HOME" subtitle="This is Eliy'a HOME page" />
       {user ? (
         <>
           <h1>Welcome {user.fullName}</h1>
